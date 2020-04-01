998c8a366c1e891dd89612e5f0b784940eae0f8d
Matt Carmody <matt.carmody@ovsgroup.com>
2020-04-01 17:31:18 -0400

    break homepage out into smaller components, finish styling menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.component';
+import './homepage.styles.scss';
+
+export default () => (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  )
