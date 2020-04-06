5db8f84bbb20749396302251fe4d44c4f29548ca
Matt Carmody <matt.carmody@ovsgroup.com>
2020-04-06 11:30:37 -0400

    update menu-items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss'
 
-const MenuItem = ({title, id, imageUrl, size}) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({title, id, imageUrl, linkUrl, size, history, match}) => (
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div className="background-image"
       style={{
         backgroundImage: `url(${imageUrl})`
@@ -15,4 +16,4 @@ const MenuItem = ({title, id, imageUrl, size}) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
-export default () => (
-    <div className='homepage'>
-      <Directory />
-    </div>
-  )
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
